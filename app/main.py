bdd0735923685fe70d5c2ec035a09a596f3a1d8a
Theo Lafforgue <theo.lafforgue@arbounie.nl>
2023-04-03 15:45:07 +0200

    phase 4 try 1

--- app/main.py
+++ app/main.py
@@ -1,16 +1,25 @@
 import subprocess
 import sys
+import os
+import tempfile
+import shutil
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    sys.stdout.write(completed_process.stdout.decode("utf-8"))
-    sys.stderr.write(completed_process.stderr.decode("utf-8"))
+    with tempfile.TemporaryDirectory as temp_dir:
+        os.makedirs(os.path.join(temp_dir, os.path.dirname(command).strip("/")))
+        shutil.copy(command, os.path.join(temp_dir, command.strip("/")))
+        os.chroot(temp_dir)
 
-    sys.exit(completed_process.returncode)
+        completed_process = subprocess.run([command, *args], capture_output=True)
+    
+        sys.stdout.write(completed_process.stdout.decode("utf-8"))
+        sys.stderr.write(completed_process.stderr.decode("utf-8"))
+
+        sys.exit(completed_process.returncode)
 
 if __name__ == "__main__":
     main()
