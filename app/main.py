8365873c8a0d52eea53a27f8ececf4b2344906e3
Theo Lafforgue <theo.lafforgue@arbounie.nl>
2023-04-06 08:37:17 +0200

    phase 5 try 2

--- app/main.py
+++ app/main.py
@@ -3,19 +3,21 @@ import sys
 import os
 import tempfile
 import shutil
-
+import ctypes
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     
     with tempfile.TemporaryDirectory() as temp_dir:
+        CLONE_NEWPID = 0x20000000
+        libc = ctypes.CDLL(None)
+        libc.unshare.argtypes = [ctypes.c_int]
+        libc.unshare(CLONE_NEWPID)
+
         os.makedirs(os.path.join(temp_dir, os.path.dirname(command).strip("/")))
         shutil.copy(command, os.path.join(temp_dir, command.strip("/")))
         os.chroot(temp_dir)
-
-        pid = os.getpid()
-        os.unshare(os.CLONE_NEWPID)
 
         completed_process = subprocess.run([command, *args], capture_output=True)
     
